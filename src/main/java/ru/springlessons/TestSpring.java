a315548318dca4fdd72330d496d9f5ba8268fb74
User <dennzelq@gmail.com>
2022-08-05 13:55:05 +0500

    initial commit

--- src/main/java/ru/springlessons/TestSpring.java
+++ src/main/java/ru/springlessons/TestSpring.java
@@ -6,11 +6,11 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//        MusicPlayer player = new MusicPlayer(music);
 
-        MusicPlayer player = new MusicPlayer(music);
-        player.playMusic();
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();
         context.close();
     }
 }
