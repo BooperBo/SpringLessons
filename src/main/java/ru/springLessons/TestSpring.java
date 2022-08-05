894a23b493402b8873468ed6a730e382846eeede
User <dennzelq@gmail.com>
2022-08-05 12:57:57 +0500

    initial commit

---
+++ src/main/java/ru/springLessons/TestSpring.java
@@ -0,0 +1,16 @@
+package ru.springLessons;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml");
+
+        TestBean testBean = context.getBean("TestBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
